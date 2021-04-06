b7319824f1e0b4c85fa99b069f25e94245d95627
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-04-06 03:02:43 +0000

    remove panel index

--- webroot/panel/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "../../resources/autoload.php";
-
-require_once config::PATHS["templates"] . "/header.php";
-include config::PATHS["templates"] . "/messages.php";
-require_once config::PATHS["templates"] . "/footer.php";
-?>
