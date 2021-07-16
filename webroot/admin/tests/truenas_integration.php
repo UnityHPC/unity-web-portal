47710b2704b0c40fbd4a6a795ffa20f046d3d5ee
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-07-16 20:02:34 +0000

    added truenas test

---
+++ webroot/admin/tests/truenas_integration.php
@@ -0,0 +1,8 @@
+<?php
+
+require "../../../resources/autoload.php";
+
+$storage = new unityStorage();
+
+$storage->createHomeDirectory("hsaplakoglu_umass_edu");
+//$storage->deleteHomeDirectory("hsaplakoglu_umass_edu");
