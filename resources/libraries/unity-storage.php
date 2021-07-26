32d31bdda6f19837510392099ef1f341b8f40a6e
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-07-26 15:35:55 +0000

    added populatehomedirectory method, untested

--- resources/libraries/unity-storage.php
+++ resources/libraries/unity-storage.php
@@ -97,10 +97,6 @@ class unityStorage
         curl_close($curl);  // close session
     }
 
-    public function populateHomeDirectory($user) {
-
-    }
-
     public function deleteHomeDirectory($user)
     {
         // This method should be redone for different deployments
@@ -140,5 +136,29 @@ class unityStorage
 
     public function getHomeDirectory($user) {
         throw new Exception("Not yet implemented");
+    }
+
+    public function populateHomeDirectory($user) {
+        $source_dir = config::DOCS_ROOT . "/etc/skel/home";
+        $source_dir_obj = new FilesystemIterator($source_dir);
+
+        $dest_dir = config::STORAGE["home_location"] . "/" . $user;
+
+        foreach ($source_dir_obj as $file) {
+            $filename = $file->getFilename();
+            $destination = $dest_dir . "/" . $filename;
+            copy($source_dir . "/" . $filename, $destination);
+            chown($destination, $user);
+            chgrp($destination, $user);
+        }
+        
+        //create scratch space
+        $scratch_web_location = config::STORAGE["scratch_web_mount"] . "/" . $user;
+        mkdir($scratch_web_location, 0700, true);
+        chgrp($scratch_web_location, $user);
+        chown($scratch_web_location, $user);
+
+        // create scratch space sym link
+        symlink(config::STORAGE["scratch_mount"] . "/" . $user, $dest_dir . "/scratch");
     }
 }
