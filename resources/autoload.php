b37cdbee598cec06e91234d118031c640d5fdec8
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-10-16 05:53:00 +0000

    added unityfs service

--- resources/autoload.php
+++ resources/autoload.php
@@ -18,15 +18,13 @@ if (config::MAINTENANCE_MODE && !isset($_SESSION["maint"]) && !(strpos($_SERVER[
 require_once config::PATHS["templates"] . "/globals.php";
 
 require_once config::PATHS["libraries"] . "/slurm.php";
+require_once config::PATHS["libraries"] . "/unityfs.php";
 require_once config::PATHS["libraries"] . "/unity-ldap.php";
 require_once config::PATHS["libraries"] . "/unity-user.php";
 //require_once config::PATHS["libraries"] . "/unity-account.php";
 require_once config::PATHS["libraries"] . "/unity-sql.php";
 require_once config::PATHS["libraries"] . "/unity-group.php";
 require_once config::PATHS["libraries"] . "/template_mailer.php";
-require_once config::PATHS["libraries"] . "/storage_drivers/storageDriver.php";
-require_once config::PATHS["libraries"] . "/storage_drivers/localStorageDriver.php";
-require_once config::PATHS["libraries"] . "/storage_drivers/truenasCoreStorageDriver.php";
 
 require_once config::PATHS["libraries"] . "/unity-service.php";
 
@@ -35,10 +33,7 @@ $SERVICE->add_ldap(config::LDAP);
 $SERVICE->add_sql(config::SQL);
 $SERVICE->add_mail(config::MAIL);
 $SERVICE->add_sacctmgr(config::SLURM);
-
-foreach (config::STORAGE_DEVICES as $name => $device) {
-  $SERVICE->add_storage($device, $name);
-}
+$SERVICE->add_unityfs(config::UNITYFS);
 
 if (isset($_SERVER["REMOTE_USER"])) {  // Check if SHIB is enabled on this page
   // Set Shibboleth Session Vars - Vars stored in session to be accessible outside shib-controlled areas of the sites (ie contact page)
@@ -51,6 +46,8 @@ if (isset($_SERVER["REMOTE_USER"])) {  // Check if SHIB is enabled on this page
   );
   $_SESSION["SHIB"] = $SHIB;  // Set the session var for non-authenticated pages
 }
+
+$USER = new unityUser($SHIB["netid"], $SERVICE);
 
 // Load Locale
 require_once config::PATHS["locale"] . "/" . config::LOCALE . ".php";  // Loads the locale class
