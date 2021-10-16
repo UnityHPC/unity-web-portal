45fa2c1c3da89d1a50dddcc5c777650f53ee3987
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-10-16 09:30:37 +0000

    the start of the faq

--- webroot/panel/support.php
+++ webroot/panel/support.php
@@ -2,144 +2,33 @@
 require "../../resources/autoload.php";
 
 require_once config::PATHS["templates"] . "/header.php";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Form was submitted
-    $subject = $_POST["subject"];
-    $prio = $_POST["prio"];
-    $type = $_POST["type"];
-
-    if (empty($subject) || empty($prio) || empty($type)) {
-        die();
-    }
-
-    $mail_data = array(
-        "mail" => $user->getMail(),
-        "name" => $user->getName(),
-        "subject" => $subject,
-        "message" => ""
-    );
-
-    switch ($type) {
-        case "soft":
-            $soft_link = $_POST["soft-link"];
-            $soft_details = $_POST["soft-details"];
-
-            if (empty($soft_link)) {
-                die();
-            }
-
-            $mail_data["message"] = "<p>Software Request</p><p><b>Link to Software: </b><a href='$soft_link'>$soft_link</a></p><p>Description of Software (optional): $soft_details</p>";
-
-            break;
-        case "bug":
-            $bug_issue = $_POST["bug-issue"];
-            $bug_steps = $_POST["bug-reproduce"];
-
-            if (empty($bug_issue) || empty($bug_steps)) {
-                die();
-            }
-
-            break;
-        case "feat":
-            $feat_message = $_POST["feat-message"];
-
-            if (empty($feat_message)) {
-                die();
-            }
-
-            break;
-        case "gen":
-            $gen_message = $_POST["gen-message"];
-
-            if (empty($gen_message)) {
-                die();
-            }
-
-
-
-            break;
-    }
-}
 ?>
 
-<h1>Request Support</h1>
+<h1>Support</h1>
 
-<p>Before submitting a ticket, please reference our documentation and FAQ page for a potential answer.</p>
+<section id="faq">
 
-<form id="contact-form" action="" method="POST" onSubmit="return confirm('<?php echo unity_locale::CONTACT_WARN_SEND; ?>');">
-    <input type="text" name="subject" placeholder="<?php echo unity_locale::LABEL_SUBJECT; ?>">
+<p><strong>How do I connect to, and start using the cluster?</strong></p>
+<p>Refer to connection instructions on our documentation page <a href="https://unity.rc.umass.edu/docs/#connecting/ssh/">here</a>. You can connect over SSH or JupyterLab.</p>
 
-    <select required="true" name="prio" form="contact-form">
-        <option disabled selected value>-- Select Priority --</option>
-        <option value="1">Critical</option>
-        <option value="2">High</option>
-        <option value="3">General</option>
-    </select>
+<p><strong>When I connect over SSH I get a message saying "permission denied (public key)"</strong></p>
+<p>This can be due to a few reasons:</p>
+<ul>
+    <li>You have not provided your public key while connecting. Using <pre>ssh -i [private_key_location] [user]@unity.rc.umass.edu should help.</li>
+    <li>You are not assigned to at least 1 PI group. We require at least 1 PI to endorse your account before you can use the cluster. Request to join a PI on the <a href="<?php echo config::PREFIX; ?>/panel/groups.php">My PIs</a> page.</li>
+    <li>You have not added a public key to your account on Unity yet. You can do this on the <a href="<?php echo config::PREFIX; ?>/panel/account.php">Account Settings</a> page.</li>
+</ul>
 
-    <select required="true" name="type" form="contact-form">
-        <option disabled selected value>-- Select Support Type --</option>
-        <option value="soft">Software Request</option>
-        <option value="bug">Bug Report</option>
-        <option value="feat">Feature Request</option>
-        <option value="gen">General Inquiry</option>
-    </select>
+<p><strong>Where can I find software to use on the cluster?</strong></p>
+<p>Most of our software is package installed and is available by default. Many other software which have different versions are available as modules. The command <pre>module av</pre> will print all available modules. Then you can use <pre>module load [name]</pre> to load a module and have access to its binaries.</p>
 
-    <input type="submit" value="Submit Ticket">
-</form>
+<p><strong>How much storage do I get on Unity and is it backed up?</strong></p>
+<p>Your home directory <pre>/home/[user]</pre> has 500GB of storage which can be expanded on request. Your scratch space <pre>/scratch/[user]</pre> has unlimited storage, but inactive files beyond 90 days will be auto-deleted without warning.</p>
+<p>We do not provide backup solutions by default. We take a snapshot of all storage at 1 AM every day for the past 48 hours. This way, if you accidentally deleted something it wouldn't be difficult to get it back within that time frame.</p>
 
-<script>
-    // Define Form Sections
-    var cfGeneral = `
-    <div class="formSection" id="cfGeneral">
-        <textarea required="true" placeholder="<?php echo unity_locale::LABEL_MESSAGE; ?>" name="gen-message" form="contact-form"></textarea>
-    </div>
-    `;
-    var cfSoftware = `
-    <div class="formSection" id="cfSoftware">
-        <input required="true" type="text" name="soft-link" placeholder="Link to requested software">
-        <textarea required="false" placeholder="Additional details about the requested software, such as required build parameters (optional)" name="soft-details" form="contact-form"></textarea>
-    </div>
-    `;
-    var cfBug = `
-    <div class="formSection" id="cfBug">
-        <textarea required="true" placeholder="Describe the issue you are encountering" name="bug-issue" form="contact-form"></textarea>
-        <textarea required="true" placeholder="Describe the steps to reproduce this bug report" name="bug-reproduce" form="contact-form"></textarea>
-    </div>
-    `;
-    var cfFeature = `
-    <div class="formSection" id="cfFeature">
-        <textarea required="true" placeholder="Describe the feature request and a possible implementation" name="feat-message" form="contact-form"></textarea>
-    </div>
-    `;
-    var afterSelector = "#contact-form > select[name=type]";
+<h2>Any more questions, bug reports, software requests, etc.? Email us at <a href="mailto:hpc@umass.edu">hpc@umass.edu</a>.</h2>
 
-    function resetForm() {
-        $("#contact-form > div.formSection").remove();
-        $("#contact-form > input[type=submit]").hide();
-    }
-
-    $("#contact-form select[name=type]").change(function() {
-        resetForm();
-
-        switch ($(this).val()) {
-            case "soft":
-                $(afterSelector).after(cfSoftware);
-                break;
-            case "bug":
-                $(afterSelector).after(cfBug);
-                break;
-            case "feat":
-                $(afterSelector).after(cfFeature);
-                break;
-            case "gen":
-                $(afterSelector).after(cfGeneral);
-                break;
-        }
-
-        $("#contact-form > input[type=submit]").show();
-    });
-</script>
+</section>
 
 <?php
 printMessages($errors, unity_locale::CONTACT_MES_SENT);
