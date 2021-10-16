1ce5bc672c272893cc4953aa8598693a3b16ecc8
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-10-16 06:48:34 +0000

    updated account page with new classes

--- webroot/panel/account.php
+++ webroot/panel/account.php
@@ -21,27 +21,24 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 break;
             case "github":
                 $gh_user = $_POST["gh_user"];
-
-                $keys = $github->getAPI("/users/$gh_user/keys");
-
-                foreach ($keys as $key) {
-                    array_push($added_keys, $key["key"]);
-                }
+                $keys = getGithubKeys($gh_user);
+                $added_keys = $keys;
                 break;
         }
 
         if (!empty($added_keys)) {
-            $totalKeys = array_merge($user->getSSHKeys(), $added_keys);
-            $user->setSSHKeys($totalKeys);
+            $added_keys = removeTrailingWhitespace($added_keys);
+            $totalKeys = array_merge($USER->getSSHKeys(), $added_keys);
+            $USER->setSSHKeys($totalKeys);
         }
     } elseif (isset($_POST["delIndex"])) {
-        $keys = $user->getSSHKeys();
+        $keys = $USER->getSSHKeys();
         unset($keys[intval($_POST["delIndex"])]);  // remove key from array
         $keys = array_values($keys);
 
-        $user->setSSHKeys($keys);  // Update user keys
+        $USER->setSSHKeys($keys);  // Update user keys
     } elseif (isset($_POST["loginshell"])) {
-        $user->setLoginShell($_POST["loginshell"]);
+        $USER->setLoginShell($_POST["loginshell"]);
     }
 }
 ?>
@@ -49,18 +46,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <h1><?php echo unity_locale::ACCOUNT_HEADER_MAIN; ?></h1>
 
 <div class="pageTop">
-    <?php
-    if (!$user->isActive()) {
-        echo "<span class='notice'><b>Notice</b> Your account is currently <b>NOT ACTIVE</b>. You will not be able to login via SSH or JupyterLab until you are a member of at least one PI group.</span>";
-    }
-    ?>
     <p>Any changes made on this page may take a few minutes to take effect on Unity.</p>
-    <pre>ssh -i [downloaded key] <?php echo $user->getUID(); ?>@unity.rc.umass.edu</pre>
+    <pre>ssh -i [downloaded key] <?php echo $USER->getUID(); ?>@unity.rc.umass.edu</pre>
 </div>
 
 <label>SSH Keys</label>
 <?php
-$sshPubKeys = $user->getSSHKeys();  // Get ssh public key attr
+$sshPubKeys = $USER->getSSHKeys();  // Get ssh public key attr
 for ($i = 0; $sshPubKeys != null && $i < count($sshPubKeys); $i++) {  // loop through keys
     echo "<div class='key-box'><textarea spellcheck='false' readonly>" . $sshPubKeys[$i] . "</textarea><form action='' method='POST'><input type='submit' class='btnRemove' value='&times;'><input type='hidden' name='delIndex' value='$i'></form></div>";
 }
@@ -71,11 +63,8 @@ for ($i = 0; $sshPubKeys != null && $i < count($sshPubKeys); $i++) {  // loop th
 <hr>
 
 <?php
-// only allow changing login shell if user is active
-if ($user->isActive()) {
-    echo "<label>Login Shell</label><br>";
-    echo "<div class='inline'><form action='' method='POST'><input type='text' name='loginshell' placeholder='Login Shell (ie. /bin/bash)' value=" . $user->getLoginShell() . "><input type='submit' value='Set Login Shell'></form></div>";
-}
+echo "<label>Login Shell</label><br>";
+echo "<div class='inline'><form action='' method='POST'><input type='text' name='loginshell' placeholder='Login Shell (ie. /bin/bash)' value=" . $USER->getLoginShell() . "><input type='submit' value='Set Login Shell'></form></div>";
 ?>
 
 <div>
@@ -85,6 +74,12 @@ if ($user->isActive()) {
     }
     ?>
 </div>
+
+<?php
+// GYPSUM GOES HERE
+//echo "<label>Cluster Access</label><br>";
+//echo "";
+?>
 
 <script>
     $("button.btnAddKey").click(function() {
