95f26001dcf3681e580c8d67955dfe8d7ced8ee9
hakasapl <hakansaplakog@gmail.com>
2022-03-31 03:44:29 +0000

    removed scripts and test from admin

--- webroot/admin/tests/github_keys.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require "../../../resources/autoload.php";
-
-var_dump(getGithubKeys("hakasapl"));
--- webroot/admin/tests/truenas_integration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "../../../resources/autoload.php";
-
-$storage = new unityStorage();
-
-//$storage->createHomeDirectory("hsaplakoglu_umass_edu");
-$storage->deleteHomeDirectory("hsaplakoglu_umass_edu");
