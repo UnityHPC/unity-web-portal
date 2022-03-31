4c4061aabaa700913254e49a0c4e645590ac3336
hakasapl <hakansaplakog@gmail.com>
2022-03-31 03:40:19 +0000

    added URI branding

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -3,10 +3,11 @@
 <footer>
   <div id="footerLogos">
     <img draggable="false" title="University of Massachusetts Amherst" src="<?php echo config::PREFIX; ?>/res/umass.png">
+    <img draggable="false" title="University of Rhode Island" src="<?php echo config::PREFIX; ?>/res/uri.png">
     <img draggable="false" title="Massachusetts Green High Performance Computing Center" src="<?php echo config::PREFIX; ?>/res/mghpcc.png">
   </div>
   <div class="footerBlock">
-    <span>Copyright &copy;<?php echo date("Y") . " " . config::CLUSTER["org"]; ?></span>&nbsp;&nbsp;<a href="/priv.php">Site Policy</a> - <a target="_blank" href="<?php echo config::APP["repo"]; ?>"><?php echo config::APP["version"]; ?></a>
+    <span>Unity Web Portal Version <?php echo config::APP["version"]; ?> (<a target="_blank" href="<?php echo config::APP["repo"]; ?>">Source</a>)</span>&nbsp;&nbsp;<a href="<?php echo config::PREFIX; ?>/priv.php">Site Policy</a>
   </div>
 </footer>
 
