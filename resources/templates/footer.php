6cfcc053504493a82596fb4798eef4fd12ba917e
Hakan Saplakoglu <hakansaplakog@gmail.com>
2022-04-04 07:39:17 +0000

    added links to footer logos

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -2,9 +2,9 @@
 
 <footer>
   <div id="footerLogos">
-    <img draggable="false" title="University of Massachusetts Amherst" src="<?php echo config::PREFIX; ?>/res/umass.png">
-    <img draggable="false" title="University of Rhode Island" src="<?php echo config::PREFIX; ?>/res/uri.png">
-    <img draggable="false" title="Massachusetts Green High Performance Computing Center" src="<?php echo config::PREFIX; ?>/res/mghpcc.png">
+    <a href="https://www.umass.edu/" target="_blank"><img draggable="false" title="University of Massachusetts Amherst" src="<?php echo config::PREFIX; ?>/res/umass.png"></a>
+    <a href="https://www.uri.edu/" target="_blank"><img draggable="false" title="University of Rhode Island" src="<?php echo config::PREFIX; ?>/res/uri.png"></a>
+    <a href="https://www.mghpcc.org/" target="_blank"><img draggable="false" title="Massachusetts Green High Performance Computing Center" src="<?php echo config::PREFIX; ?>/res/mghpcc.png"></a>
   </div>
   <div class="footerBlock">
     <span>Unity Web Portal Version <?php echo config::APP["version"]; ?> (<a target="_blank" href="<?php echo config::APP["repo"]; ?>">Source</a>)</span>&nbsp;&nbsp;<a href="<?php echo config::PREFIX; ?>/priv.php">Site Policy</a>
