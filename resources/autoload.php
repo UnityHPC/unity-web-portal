9964b8f821bdf1831bfa4ed6a0aa358304b2138d
hakasapl <hakansaplakog@gmail.com>
2022-09-19 11:59:15 -0400

    disabled unityLogger

--- resources/autoload.php
+++ resources/autoload.php
@@ -7,10 +7,6 @@ require_once __DIR__ . "/../vendor/autoload.php";
 // Load config file values
 $CONFIG = parse_ini_file(__DIR__ . "/../config/config.ini", true);
 
-// Load unity logger program
-require_once __DIR__ . "/lib/UnityLogger.php";
-$LOGGER = new UnityLogger($CONFIG["site"]["log_path"], false);
-
 // set relative path
 if ($CONFIG["site"]["prefix"] == "") {
   define("REL_PATH", $_SERVER['REQUEST_URI']);
@@ -29,16 +25,8 @@ require_once __DIR__ . "/lib/UnityGroup.php";
 require_once __DIR__ . "/lib/UnitySQL.php";
 require_once __DIR__ . "/lib/UnityMailer.php";
 
-// Loading branding
-$branding_file_loc = __DIR__ . "/../config/branding";
-$BRANDING = parse_ini_file($branding_file_loc . "/config.ini", true);
-
-define("DOMAIN", $_SERVER['HTTP_HOST']);
-$branding_override = $branding_file_loc . "/overrides/" . DOMAIN . ".ini";
-if (file_exists($branding_override)) {
-    $override_config = parse_ini_file($branding_override);
-    $BRANDING = array_merge($BRANDING, $override_config);
-}
+// initialize branding
+require_once __DIR__ . "/run/branding.php";
 
 //
 // Initialize Service Stack
@@ -74,30 +62,16 @@ $CONFIG["smtp"]["port"],
 $CONFIG["smtp"]["security"],
 $CONFIG["smtp"]["user"],
 $CONFIG["smtp"]["pass"],
-$CONFIG["smtp"]["ssl_verify"]
+$CONFIG["smtp"]["ssl_verify"],
+$BRANDING["site"]["url"] . $CONFIG["site"]["prefix"],
+$BRANDING["mail"]["sender"],
+$BRANDING["mail"]["sender_name"],
+$BRANDING["mail"]["support"],
+$BRANDING["mail"]["support_name"]
 );
 
-$LOGGER->logInfo("Accepted connection from " . $_SERVER['REMOTE_ADDR']);
+require_once __DIR__ . "/run/sso.php";
 
-if (isset($_SERVER["REMOTE_USER"])) {  // Check if SSO is enabled on this page
-  // Set SSO Session Vars - Vars stored in session to be accessible outside shib-controlled areas of the sites (ie contact page)
-  $SSO = array(
-    "user" => EPPN_to_uid($_SERVER["REMOTE_USER"]),
-    "firstname" => $_SERVER["givenName"],
-    "lastname" => $_SERVER["sn"],
-    "name" => $_SERVER["givenName"] . " " . $_SERVER["sn"],
-    "mail" => isset($_SERVER["mail"]) ? $_SERVER["mail"] : $_SERVER["eppn"]  // Fallback to EPPN if mail is not set
-  );
-  $_SESSION["SSO"] = $SSO;  // Set the session var for non-authenticated pages
-
-  // define user object
-  $USER = new UnityUser($SSO["user"], $LDAP, $SQL, $MAILER);
-  $_SESSION["user_exists"] = $USER->exists();
-  $_SESSION["is_pi"] = $USER->isPI();
-  $_SESSION["is_admin"] = $USER->isAdmin();
-}
-
-// TODO FIX THIS
 define("LOC_HEADER", __DIR__ . "/templates/header.php");
 define("LOC_FOOTER", __DIR__ . "/templates/footer.php");
 
