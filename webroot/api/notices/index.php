876f7d63a4e89563775b73f27aa7cab392657db4
hakasapl <hakansaplakog@gmail.com>
2022-09-19 12:01:24 -0400

    added rudimentary API for cluster notices

---
+++ webroot/api/notices/index.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-type: text/plain');
+
+require "../../../resources/autoload.php";
+
+if (isset($_GET["line_wrap"])) {
+    $CHAR_WRAP = $_GET["line_wrap"];
+} else {
+    $CHAR_WRAP = 80;
+}
+
+$notices = $SQL->getNotices();
+foreach($notices as $notice) {
+    echo $notice["title"] . "\r\n";
+    echo date('m-d-Y', strtotime($notice["date"])) . "\r\n";
+
+    $lineArr = explode("\r\n", wordwrap($notice["message"], $CHAR_WRAP));
+    foreach ($lineArr as $line) {
+        echo $line;
+    }
+
+    echo "\r\n\r\n";
+}
