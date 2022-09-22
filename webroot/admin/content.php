e42785bfce85ba0a20e5c823b3f9c96e44874ad6
hakasapl <hakansaplakog@gmail.com>
2022-09-22 12:47:29 -0400

    added content edit page

--- webroot/admin/content.php
+++ webroot/admin/content.php
@@ -6,7 +6,9 @@ if (!$USER->isAdmin()) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    if (!empty($_POST["pageSel"])) {
+        $SQL->editPage($_POST["pageSel"], $_POST["content"]);
+    }
 }
 
 include LOC_HEADER;
@@ -15,6 +17,45 @@ include LOC_HEADER;
 <h1>Page Content Management</h1>
 <hr>
 
+<form id="pageForm" method="POST" action="">
+    <select name="pageSel" required>
+        <option value="" selected disabled hidden>Select page...</option>
+        <?php
+        $pages = $SQL->getPages();
+
+        foreach ($pages as $page) {
+            echo "<option value='" . $page["page"] . "'>" . $page["page"] . "</option>";
+        }
+        ?>
+    </select>
+
+    <textarea name="content" id="editor" form="pageForm"></textarea>
+
+    <input type="submit" value="Edit Page">
+</form>
+
+<script>
+    ClassicEditor
+        .create(document.querySelector('#editor'), {})
+        .then(editor => {
+            mainEditor = editor;
+        })
+        .catch(error => {
+            console.error(error)
+        });
+
+    $("#pageForm > select[name=pageSel]").change(function(e) {
+        $.ajax({url: "<?php echo $CONFIG["site"]["prefix"] ?>/admin/ajax/get_page_contents.php?pageid=" + $(this).val(), success: function(result) {
+            mainEditor.setData(result);
+        }});
+    });
+
+    $("#pageForm").on("submit", function(e) {
+        if (!confirm("Are you sure you want to edit this page?")) {
+            e.preventDefault();
+        }
+    })
+</script>
 
 <?php
 include LOC_FOOTER;
