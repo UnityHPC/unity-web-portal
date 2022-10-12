27b20c20285febdd97c1871da823c00518a27004
hakasapl <hakansaplakog@gmail.com>
2022-10-12 12:25:55 -0400

    fixed dev docker images not starting on windows

--- tools/docker-dev/identity/process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# start apache2
-apache2ctl -D FOREGROUND &
-
-# start SLAPD
-slapd -h "ldap:/// ldapi:///" -u openldap -g openldap -d 0
--- tools/docker-dev/sql/process.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apache2ctl -D FOREGROUND &
-
-mysqld
