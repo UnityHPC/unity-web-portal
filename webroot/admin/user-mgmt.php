ec558ed845cfe2ae7b90cb99279b62d8d37dd5cb
Sean Mckeag <mckeags7@gmail.com>
2022-10-20 12:04:07 -0400

    Update user-mgmt.php

--- webroot/admin/user-mgmt.php
+++ webroot/admin/user-mgmt.php
@@ -30,7 +30,7 @@ include $LOC_HEADER;
         <td>Name</td>
         <td>Unity ID</td>
         <td>Org</td>
-        <td>PIGroup</td>
+        <td>PIGroups</td>
         <td>Mail</td>
         <td>Actions</td>
     </tr>
