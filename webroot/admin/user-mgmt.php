baece3dc45bafa45c688e84f7fd1ea6414a83693
Sean Mckeag <mckeags7@gmail.com>
2022-10-20 15:16:43 -0400

    Revert "Update user-mgmt.php"
    This reverts commit ec558ed845cfe2ae7b90cb99279b62d8d37dd5cb.

--- webroot/admin/user-mgmt.php
+++ webroot/admin/user-mgmt.php
@@ -30,7 +30,7 @@ include $LOC_HEADER;
         <td>Name</td>
         <td>Unity ID</td>
         <td>Org</td>
-        <td>PIGroups</td>
+        <td>PIGroup</td>
         <td>Mail</td>
         <td>Actions</td>
     </tr>
