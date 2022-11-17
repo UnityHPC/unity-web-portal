aa30e6aeff2b2d79cc77f47644413158ac2fcfae
Hakan Saplakoglu <hakansaplakog@gmail.com>
2022-11-16 22:10:49 -0500

    fixed phpcs linting in user-mgmt.php

--- webroot/admin/user-mgmt.php
+++ webroot/admin/user-mgmt.php
@@ -46,7 +46,7 @@ include $LOC_HEADER;
         echo "<td><a href='mailto:" . $user->getMail() . "'>" . $user->getMail() . "</a></td>";
         echo "<td>";
         $cur_user_groups = $user->getGroups();
-        foreach($cur_user_groups as $cur_group) {
+        foreach ($cur_user_groups as $cur_group) {
             echo "<a href='mailto:" . $cur_group->getOwner()->getMail() . "'>" . $cur_group->getPIUID() . "</a>";
             if ($cur_group !== array_key_last($cur_user_groups)) {
                 echo '<br>';
