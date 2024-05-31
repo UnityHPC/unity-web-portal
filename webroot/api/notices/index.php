98d6b4568ff9f725a05820674a9469ecf6534860
Shaswat Ganisshan <sganisshan13@gmail.com>
2024-05-30 22:19:19 -0400

    removed wordwrap

--- webroot/api/notices/index.php
+++ webroot/api/notices/index.php
@@ -4,19 +4,13 @@ header('Content-type: text/plain');
 
 require_once "../../../resources/autoload.php";
 
-if (isset($_GET["line_wrap"])) {
-    $CHAR_WRAP = $_GET["line_wrap"];
-} else {
-    $CHAR_WRAP = 80;
-}
-
 $notices = $SQL->getNotices();
 $jsonArray = [];
 foreach ($notices as $notice) {
     $formattedNotice = [
         "title" => $notice["title"],
         "date" => date('m-d-Y', strtotime($notice["date"])),
-        "message" => wordwrap($notice["message"], $CHAR_WRAP)
+        "message" => $notice["message"]
     ];
     $jsonArray[] = $formattedNotice;
 }
