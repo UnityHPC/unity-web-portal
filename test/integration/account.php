36a66d4ceeaeebb9e358bb0a80156e9741adcc82
Simon Leary <simon.leary42@proton.me>
2025-03-28 19:40:55 -0400

    make functions

--- test/integration/account.php
+++ test/integration/account.php
@@ -41,6 +41,37 @@ function post(string $phpfile, array $post_data): string {
     return $output;
 }
 
+function delete_ssh_key(int $index): void {
+    post(
+        "../../webroot/panel/account.php",
+        ["form_type" => "delKey", "delIndex" => $index]
+    );
+}
+
+function add_ssh_key_paste(string $key): void {
+    post(
+        "../../webroot/panel/account.php",
+        [
+            "form_type" => "addKey",
+            "add_type" => "paste",
+            "key" => $key
+        ]
+    );
+}
+
+function add_ssh_key_import(string $key): void {
+    $tmp = tmpfile();
+    $tmp_path = stream_get_meta_data($tmp)["uri"];
+    fwrite($tmp, $key);
+    $_FILES["keyfile"] = ["tmp_name" => $tmp_path];
+    post(
+        "../../webroot/panel/account.php",
+        ["form_type" => "addKey", "add_type" => "import"]
+    );
+    unlink($tmp_path);
+    unset($_FILES["keyfile"]);
+}
+
 $pi_1 = ["user1@org1.test", "Givenname", "Surname", "user1@org1.test"];
 $user_in_pi_1 = ["user2@org1.test", "Givenname", "Surname", "user2@org1.test"];
 $new_user = ["FIXME", "Givenname", "Surname", "FIXME"];
@@ -51,77 +82,45 @@ switch_to_user(...$user_in_pi_1);
 
 $initial_ssh_keys = $USER->getSSHKeys(true);
 
-// test paste valid key twice and then delete
+// test paste valid key that doesn't exist yet
 $new_key_index = count($USER->getSSHKeys(true));
 assert(!in_array($valid_ssh_key, $USER->getSSHKeys(true)));
-post(
-    "../../webroot/panel/account.php",
-    [
-        "form_type" => "addKey",
-        "add_type" => "paste",
-        "key" => $valid_ssh_key
-    ]
-);
+add_ssh_key_paste($valid_ssh_key);
 assert(in_array($valid_ssh_key, $USER->getSSHKeys(true)));
+
+// test paste valid key that already exists
 $count_before_duplicate_add = count($USER->getSSHKeys(true));
-post(
-    "../../webroot/panel/account.php",
-    [
-        "form_type" => "addKey",
-        "add_type" => "paste",
-        "key" => $valid_ssh_key
-    ]
-);
+assert($count_before_duplicate_add > 0);
+add_ssh_key_paste($valid_ssh_key);
 assert(count($USER->getSSHKeys(true)) == $count_before_duplicate_add);
-post(
-    "../../webroot/panel/account.php",
-    ["form_type" => "delKey", "delIndex" => $new_key_index]
-);
+
+// cleanup
+delete_ssh_key($new_key_index);
 assert(!in_array($valid_ssh_key, $USER->getSSHKeys(true)));
 
 // test paste invalid key
 assert(!in_array($invalid_ssh_key, $USER->getSSHKeys(true)));
-post(
-    "../../webroot/panel/account.php",
-    [
-        "form_type" => "addKey",
-        "add_type" => "paste",
-        "key" => $invalid_ssh_key
-    ]
-);
+add_ssh_key_paste($invalid_ssh_key);
 assert(!in_array($invalid_ssh_key, $USER->getSSHKeys(true)));
 
-// test import valid key twice and then delete
+// test import valid key that doesn't exist yet
 assert(!in_array($valid_ssh_key, $USER->getSSHKeys(true)));
-$tmp = tmpfile();
-$tmp_path = stream_get_meta_data($tmp)["uri"];
-fwrite($tmp, $valid_ssh_key);
-$_FILES["keyfile"] = ["tmp_name" => $tmp_path];
-post(
-    "../../webroot/panel/account.php",
-    ["form_type" => "addKey", "add_type" => "import"]
-);
-unlink($tmp_path);
-unset($_FILES["keyfile"]);
+add_ssh_key_import($valid_ssh_key);
 assert(in_array($valid_ssh_key, $USER->getSSHKeys(true)));
-post(
-    "../../webroot/panel/account.php",
-    ["form_type" => "delKey", "delIndex" => $new_key_index]
-);
+
+// test import valid key that already exists
+$count_before_duplicate_add = count($USER->getSSHKeys(true));
+assert($count_before_duplicate_add > 0);
+add_ssh_key_import($valid_ssh_key);
+assert(count($USER->getSSHKeys(true)) == $count_before_duplicate_add);
+
+// cleanup
+delete_ssh_key($new_key_index);
 assert(!in_array($valid_ssh_key, $USER->getSSHKeys(true)));
 
-// test import invalid key
+// test import valid key that doesn't exist yet
 assert(!in_array($invalid_ssh_key, $USER->getSSHKeys(true)));
-$tmp = tmpfile();
-$tmp_path = stream_get_meta_data($tmp)["uri"];
-fwrite($tmp, $invalid_ssh_key);
-$_FILES["keyfile"] = ["tmp_name" => $tmp_path];
-post(
-    "../../webroot/panel/account.php",
-    ["form_type" => "delKey", "delIndex" => $new_key_index]
-);
-unlink($tmp_path);
-unset($_FILES["keyfile"]);
+add_ssh_key_import($invalid_ssh_key);
 assert(!in_array($invalid_ssh_key, $USER->getSSHKeys(true)));
 
 // // test generate key
