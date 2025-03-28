112fedb87ceb6a4c2f00e4445b44d7b3d2c549e4
Simon Leary <simon.leary42@proton.me>
2025-03-28 03:26:50 -0400

    define site vars

--- tools/docker-dev/sql/bootstrap.sql
+++ tools/docker-dev/sql/bootstrap.sql
@@ -188,6 +188,11 @@ CREATE TABLE `sitevars` (
   `value` varchar(768) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `sitevars` (`id`, `name`, `value`) VALUES
+(0, 'MAX_UID', '0'),
+(1, 'MAX_GID', '0'),
+(2, 'MAX_PIGID', '0');
+
 --
 -- Indexes for dumped tables
 --
