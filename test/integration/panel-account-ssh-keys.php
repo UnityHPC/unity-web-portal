87fc05b6b4346668894a6b7b41cca5dc777693d1
Simon Leary <simon.leary42@proton.me>
2025-03-30 11:59:17 -0400

    more tests

--- test/integration/panel-account-ssh-keys.php
+++ test/integration/panel-account-ssh-keys.php
@@ -152,16 +152,33 @@ delete_ssh_key($new_key_index);
 assert(!in_array($valid_ssh_key, $USER->getSSHKeys(true)));
 
 // test github nonexistent account / no keys in account
-$count_before_duplicate_add = count($USER->getSSHKeys(true));
-assert($count_before_duplicate_add > 0);
+$count_before = count($USER->getSSHKeys(true));
 add_ssh_keys_github([]);
-assert(count($USER->getSSHKeys(true)) == $count_before_duplicate_add);
+assert(count($USER->getSSHKeys(true)) == $count_before);
 
 // test github invalid key
 // FIXME webroot php does not check validity
 // assert(!in_array($invalid_ssh_key, $USER->getSSHKeys(true)));
 // add_ssh_keys_github([$invalid_ssh_key]);
 // assert(!in_array($invalid_ssh_key, $USER->getSSHKeys(true)));
+
+// test delete key with negative index
+// FIXME webroot php does not check index validity
+// $count_before = count($USER->getSSHKeys(true));
+// try {
+//     delete_ssh_key(-1);
+//     assert(false, 'delete_ssh_key(-1) should have failed but didnt!');
+// } catch(Exception) {}
+// assert(count($USER->getSSHKeys(true)) == $count_before);
+
+// test delete key with index too large
+// FIXME webroot php does not check index validity
+// $count_before = count($USER->getSSHKeys(true));
+// try {
+//     delete_ssh_key($new_key_index);
+//     assert(false, 'delete_ssh_key($new_key_index) should have failed but didnt!');
+// } catch(Exception) {}
+// assert(count($USER->getSSHKeys(true)) == $count_before);
 
 // test that everything is the way we found it
 assert($USER->getSSHKeys(true) === $initial_ssh_keys);
