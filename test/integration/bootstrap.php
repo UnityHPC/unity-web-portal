1d60102c33ab926f0cc0835974d1c490be82da7a
Simon Leary <simon.leary42@proton.me>
2025-03-30 11:29:28 -0400

    cleanup

---
+++ test/integration/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+
+$_SERVER = [
+    "REMOTE_ADDR" => "127.0.0.1"
+];
+
+require_once "../../resources/autoload.php";
+
+ini_set("assert.exception", false);
+ini_set("assert.warning", true);
+
+function switch_to_user(string $eppn, string $given_name, string $sn, string $mail): void {
+    global $CONFIG, $REDIS, $LDAP, $SQL, $MAILER, $WEBHOOK, $SITE, $SSO, $OPERATOR, $USER, $SEND_PIMESG_TO_ADMINS, $LOC_HEADER, $LOC_FOOTER;
+    unset($SSO);
+    // unset($_SESSION);
+    session_write_close();
+    session_id(str_replace("@", "-", str_replace(".", "-", $eppn)));
+    // init.php will call session_start()
+    $_SERVER["REMOTE_USER"] = $eppn;
+    $_SERVER["eppn"] = $eppn;
+    $_SERVER["givenName"] = $given_name;
+    $_SERVER["sn"] = $sn;
+    ob_start();
+    include "../../resources/init.php";
+    $_ = ob_get_clean();
+    assert(isset($OPERATOR));
+    assert(isset($SSO));
+    assert(isset($_SESSION));
+    assert(isset($USER));
+}
+
+function post(string $phpfile, array $post_data): string {
+    global $CONFIG, $REDIS, $LDAP, $SQL, $MAILER, $WEBHOOK, $SITE, $SSO, $OPERATOR, $USER, $SEND_PIMESG_TO_ADMINS, $LOC_HEADER, $LOC_FOOTER;
+    $_SERVER["REQUEST_METHOD"] = "POST";
+    $_POST = $post_data;
+    ob_start();
+    include $phpfile;
+    $output = ob_get_clean();
+    unset($_POST);
+    unset($_SERVER["REQUEST_METHOD"]);
+    return $output;
+}
