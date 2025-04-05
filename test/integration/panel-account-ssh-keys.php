87eeffc8e421ca05895b5a30f52df8e7be8f3cf2
Simon Leary <simon.leary42@proton.me>
2025-04-04 20:11:28 -0400

    get ssh key deletion tests working

--- test/integration/panel-account-ssh-keys.php
+++ test/integration/panel-account-ssh-keys.php
@@ -8,15 +8,47 @@ use Exception;
 require_once "panel-bootstrap.php";
 
 class PanelAccountSSHKeyTest {
-    static function delete_ssh_key(int $index): string {
-        return post(
+    private static function delete_ssh_key(mixed $index): void {
+        post(
             "../../webroot/panel/account.php",
             ["form_type" => "delKey", "delIndex" => $index]
         );
     }
 
-    static function add_ssh_key_paste(string $key): string {
-        return post(
+    private static function test_delete_ssh_key(mixed $index): void {
+        global $USER, $CONFIG, $SITE;
+        $keys_before = $USER->getSSHKeys();
+        $expect_bad_request_exception = false;
+        if (is_string($index) && !preg_match("/^[0-9]+$/", $index)) {
+            $expected_keys_after = $keys_before;
+            $expect_bad_request_exception = true;
+        } elseif ($index < 0){
+            $expected_keys_after = $keys_before;
+            $expect_bad_request_exception = true;
+        } elseif ($index >= count($keys_before)){
+            $expected_keys_after = $keys_before;
+            $expect_bad_request_exception = true;
+        } else {
+            $expected_keys_after = $keys_before;
+            unset($expected_keys_after[intval($index)]);
+        }
+        ob_start();
+        try {
+            self::delete_ssh_key($index);
+        } catch (BadRequestException $e) {
+            if (!$expect_bad_request_exception) {
+                throw $e;
+            }
+        } finally {
+            $output = ob_get_clean();
+        }
+        $keys_after = $USER->getSSHKeys();
+        assert($keys_after == $expected_keys_after);
+
+    }
+
+    private static function add_ssh_key_paste(string $key): void {
+        post(
             "../../webroot/panel/account.php",
             [
                 "form_type" => "addKey",
@@ -26,13 +58,13 @@ class PanelAccountSSHKeyTest {
         );
     }
 
-    static function add_ssh_key_import(string $key): string {
+    private static function add_ssh_key_import(string $key): void {
         $tmp = tmpfile();
         $tmp_path = stream_get_meta_data($tmp)["uri"];
         fwrite($tmp, $key);
         $_FILES["keyfile"] = ["tmp_name" => $tmp_path];
         try {
-            return post(
+            post(
                 "../../webroot/panel/account.php",
                 ["form_type" => "addKey", "add_type" => "import"]
             );
@@ -42,8 +74,8 @@ class PanelAccountSSHKeyTest {
         }
     }
 
-    static function add_ssh_key_generated(string $key): string {
-        return post(
+    private static function add_ssh_key_generated(string $key): void {
+        post(
             "../../webroot/panel/account.php",
             [
                 "form_type" => "addKey",
@@ -53,13 +85,13 @@ class PanelAccountSSHKeyTest {
         );
     }
 
-    static function add_ssh_keys_github(array $keys): string {
+    private static function add_ssh_keys_github(array $keys): void {
         global $SITE;
         $old_site = $SITE;
         $SITE = Mockery::mock(UnitySite::class)->makePartial();
         $SITE->shouldReceive("getGithubKeys")->with("foobar")->andReturn($keys);
         try {
-            return post(
+            post(
                 "../../webroot/panel/account.php",
                 [
                     "form_type" => "addKey",
@@ -73,7 +105,7 @@ class PanelAccountSSHKeyTest {
         }
     }
 
-    static function test_add_ssh_keys_all_methods(array $add_keys){
+    private static function test_add_ssh_keys_all_methods(array $add_keys){
         // test adding SSH keys using all 4 methods, taking into account how many
         // keys the user already has, how many are valid, and how many the user is
         // actually allowed to upload before they hit the limit
@@ -98,28 +130,32 @@ class PanelAccountSSHKeyTest {
             "import" => "self::add_ssh_key_import",
             "generate" => "self::add_ssh_key_generated"
         ] as $func_name => $func){
+            ob_start();
             foreach($add_keys as $new_key){
                 call_user_func($func, $new_key);
             }
+            $output = ob_get_clean();
             $keys_after = $USER->getSSHKeys();
             assert(
                 $keys_after == $expected_keys_after,
-                json_encode([
-                    "method" => $func_name,
-                    "keys_before" => $keys_before,
-                    "add_keys" => $add_keys,
-                    "new_keys" => $new_keys,
-                    "new_valid_keys" => $new_valid_keys,
-                    "open_key_slots" => $open_key_slots,
-                    "expected_added_keys" => $expected_added_keys,
-                    "expected_keys_after" => $expected_keys_after,
-                    "keys_after" => $keys_after,
-                ], JSON_PRETTY_PRINT)
+                // json_encode([
+                //     "method" => $func_name,
+                //     "keys_before" => $keys_before,
+                //     "add_keys" => $add_keys,
+                //     "new_keys" => $new_keys,
+                //     "new_valid_keys" => $new_valid_keys,
+                //     "open_key_slots" => $open_key_slots,
+                //     "expected_added_keys" => $expected_added_keys,
+                //     "expected_keys_after" => $expected_keys_after,
+                //     "keys_after" => $keys_after,
+                // ], JSON_PRETTY_PRINT)
             );
             $USER->setSSHKeys($keys_before);
         }
         // github key add method takes entire list at once
+        ob_start();
         self::add_ssh_keys_github($add_keys);
+        ob_get_clean();
         // github key add method does nothing if limit is reached or any key is invalid
         if (
             (count($new_valid_keys) != count($new_keys)) || count($new_valid_keys) > $open_key_slots){
@@ -128,22 +164,22 @@ class PanelAccountSSHKeyTest {
         $keys_after = $USER->getSSHKeys();
         assert(
             $keys_after == $expected_keys_after,
-            json_encode([
-                "method" => "github",
-                "keys_before" => $keys_before,
-                "add_keys" => $add_keys,
-                "new_keys" => $new_keys,
-                "new_valid_keys" => $new_valid_keys,
-                "open_key_slots" => $open_key_slots,
-                "expected_added_keys" => $expected_added_keys,
-                "expected_keys_after" => $expected_keys_after,
-                "keys_after" => $keys_after,
-            ], JSON_PRETTY_PRINT)
+            // json_encode([
+            //     "method" => "github",
+            //     "keys_before" => $keys_before,
+            //     "add_keys" => $add_keys,
+            //     "new_keys" => $new_keys,
+            //     "new_valid_keys" => $new_valid_keys,
+            //     "open_key_slots" => $open_key_slots,
+            //     "expected_added_keys" => $expected_added_keys,
+            //     "expected_keys_after" => $expected_keys_after,
+            //     "keys_after" => $keys_after,
+            // ], JSON_PRETTY_PRINT)
         );
         $USER->setSSHKeys($keys_before);
     }
 
-    static function get_max_number_of_valid_ssh_keys(){
+    private static function get_max_number_of_valid_ssh_keys(){
         // the docker dev tooling should create some users with their account completely full of
         // unique valid SSH keys. find one of these accounts and return their keys.
         global $CONFIG, $LDAP, $SQL, $MAILER, $REDIS, $WEBHOOK;
@@ -158,21 +194,31 @@ class PanelAccountSSHKeyTest {
         throw new Exception("could not find any user with max ssh keys!");
     }
 
-    static function test_add_ssh_keys_all_methods_all_inputs(){
+    public static function test_add_ssh_keys_all_methods_all_inputs(){
         $max_number_of_valid_keys = self::get_max_number_of_valid_ssh_keys();
         $inputs = [
-            "no keys" => [],
-            "single invalid key" => ["foobar"],
-            "single valid key" => [$max_number_of_valid_keys[0]],
-            "one valid one invalid" => ["foobar", $max_number_of_valid_keys[0]],
-            "maximum number of valid keys" => $max_number_of_valid_keys,
-            "maximum number of keys, all valid except one" => array_slice($max_number_of_valid_keys, 0, -1) + ["foobar"]
+            "add keys []" => [],
+            "add single invalid key" => ["foobar"],
+            "add single valid key" => [$max_number_of_valid_keys[0]],
+            "add one valid one invalid key" => ["foobar", $max_number_of_valid_keys[0]],
+            "add maximum number of valid keys" => $max_number_of_valid_keys,
+            "add maximum number of keys, all valid except one" => array_slice($max_number_of_valid_keys, 0, -1) + ["foobar"]
         ];
         foreach ($inputs as $input_name => $input){
             echo "###############################################################\n";
             echo "$input_name\n";
-            echo "\n";
             self::test_add_ssh_keys_all_methods($input);
+            echo "\n";
+        }
+    }
+
+    public static function test_delete_ssh_keys_all_inputs(){
+        global $CONFIG;
+        $inputs = [-1, 0, intval($CONFIG["ldap"]["max_num_ssh_keys"]), "foobar"];
+        foreach ($inputs as $input){
+            echo "###############################################################\n";
+            echo "delete key index '$input'\n";
+            self::test_delete_ssh_key($input);
             echo "\n";
         }
     }
--- test/integration/panel-bootstrap.php
+++ test/integration/panel-bootstrap.php
@@ -8,6 +8,7 @@ require "../../resources/autoload.php";
 
 ini_set("assert.exception", false);
 ini_set("assert.warning", true);
+ini_set("error_log", "/dev/null");
 
 function switch_to_user(string $eppn, string $given_name, string $sn, string $mail): void {
     global $CONFIG, $REDIS, $LDAP, $SQL, $MAILER, $WEBHOOK, $SITE, $SSO, $OPERATOR, $USER, $SEND_PIMESG_TO_ADMINS, $LOC_HEADER, $LOC_FOOTER;
@@ -30,14 +31,11 @@ function switch_to_user(string $eppn, string $given_name, string $sn, string $ma
     assert(isset($SITE));
 }
 
-function post(string $phpfile, array $post_data): string {
+function post(string $phpfile, array $post_data): void {
     global $CONFIG, $REDIS, $LDAP, $SQL, $MAILER, $WEBHOOK, $SITE, $SSO, $OPERATOR, $USER, $SEND_PIMESG_TO_ADMINS, $LOC_HEADER, $LOC_FOOTER;
     $_SERVER["REQUEST_METHOD"] = "POST";
     $_POST = $post_data;
-    ob_start();
     include $phpfile;
-    $output = ob_get_clean();
     unset($_POST);
     unset($_SERVER["REQUEST_METHOD"]);
-    return $output;
 }
