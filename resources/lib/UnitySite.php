cf6072dd78363cf7705c1475d7bcef3ae9e3c34d
Simon Leary <simon.leary42@proton.me>
2025-04-07 13:40:53 -0400

    fix style

--- resources/lib/UnitySite.php
+++ resources/lib/UnitySite.php
@@ -57,7 +57,8 @@ class UnitySite
         }
     }
 
-    public function alert(string $msg): void{
+    public function alert(string $msg): void
+    {
         echo "<script type='text/javascript'>alert(" . json_encode($msg) . ");</script>";
     }
 }
