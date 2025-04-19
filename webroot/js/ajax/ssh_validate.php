e9dd8e18346cf8bdc798802043941281a86da9f1
Simon Leary <simon.leary42@proton.me>
2025-04-19 13:02:02 -0400

    require once

--- webroot/js/ajax/ssh_validate.php
+++ webroot/js/ajax/ssh_validate.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . "/../../../resources/lib/UnitySite.php";
+require_once __DIR__ . "/../../../resources/lib/UnitySite.php";
 use UnityWebPortal\lib\UnitySite;
 
 echo UnitySite::testValidSSHKey($_POST["key"]) ? "true" : "false";
