deb35a9fb57f25c7c27ea1a9e33b7872c81f2167
Simon Leary <simon.leary42@proton.me>
2025-04-19 12:27:02 -0400

    require must be absolute since included file in deeper directory than test

--- webroot/js/ajax/ssh_validate.php
+++ webroot/js/ajax/ssh_validate.php
@@ -1,8 +1,5 @@
 <?php
-
-require "../../../resources/autoload.php";
-
+require __DIR__ . "/../../../resources/autoload.php";
 use UnityWebPortal\lib\UnitySite;
-use phpseclib3\Crypt\PublicKeyLoader;
 
-echo (UnitySite::testValidSSHKey($_POST["key"]) ? "true" : "false");
+echo UnitySite::testValidSSHKey($_POST["key"]) ? "true" : "false";
