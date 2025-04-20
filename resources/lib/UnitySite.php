81a4a219640ea6168ca6b369042cac45d3ab2c5a
Simon Leary <simon.leary42@proton.me>
2025-04-19 20:51:31 -0400

    add comments, try out removing digits check

--- resources/lib/UnitySite.php
+++ resources/lib/UnitySite.php
@@ -52,19 +52,26 @@ class UnitySite
 
     public static function testValidSSHKey($key_str)
     {
-        // https://github.com/phpseclib/phpseclib/issues/2077
-        // key loader still throws exception, this just mutes a warning for phpunit
-        if (preg_match("/^[0-9]+$/", $key_str)) {
+        // key loader still throws, these just mute warnings for phpunit
+        // https://github.com/phpseclib/phpseclib/issues/2079
+        if ($key_str == "") {
             return false;
         }
+        // https://github.com/phpseclib/phpseclib/issues/2077
+        // if (preg_match("/^[0-9]+$/", $key_str)) {
+        //     return false;
+        // }
         // https://github.com/phpseclib/phpseclib/issues/2076
-        // key loader still throws exception, this just mutes a warning for phpunit
+        // there are actually valid JSON keys (JWK), but I don't think anybody uses it
         if (!is_null(@json_decode($key_str))) {
             return false;
         }
         try {
             PublicKeyLoader::load($key_str);
             return true;
+        // phpseclib should throw only NoKeyLoadedException but that is not the case
+        // https://github.com/phpseclib/phpseclib/pull/2078
+        // } catch (\phpseclib3\Exception\NoKeyLoadedException $e) {
         } catch (\Throwable $e) {
             return false;
         }
