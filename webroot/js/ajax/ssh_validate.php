6201b372464d2df99f328471d4c798cc4b87d072
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2025-04-20 16:31:20 -0400

    improve ssh key validation (#187)
    * improve ssh key validation
    
    * catch all throwables
    
    * trim should be done externally
    
    * phpunit expect warning
    
    * put back checks to please phpunit
    
    * add comments, try out removing digits check
    
    * remove digits check
    
    * remove use

--- webroot/js/ajax/ssh_validate.php
+++ webroot/js/ajax/ssh_validate.php
@@ -1,12 +1,5 @@
 <?php
 
-require "../../../resources/autoload.php";
+require_once __DIR__ . "/../../../resources/lib/UnitySite.php";
 
-use phpseclib3\Crypt\PublicKeyLoader;
-
-try {
-    PublicKeyLoader::load($_POST['key'], $password = false);
-    echo "true";
-} catch (Exception $e) {
-    echo "false";
-}
+echo UnityWebPortal\lib\UnitySite::testValidSSHKey($_POST["key"]) ? "true" : "false";
