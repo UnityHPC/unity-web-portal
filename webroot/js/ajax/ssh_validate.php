d508d0c89abd0c0b62530dec61e16f98cb21c76c
Simon Leary <simon.leary42@proton.me>
2025-04-19 20:54:22 -0400

    remove use

--- webroot/js/ajax/ssh_validate.php
+++ webroot/js/ajax/ssh_validate.php
@@ -1,6 +1,5 @@
 <?php
 
 require_once __DIR__ . "/../../../resources/lib/UnitySite.php";
-use UnityWebPortal\lib\UnitySite;
 
-echo UnitySite::testValidSSHKey($_POST["key"]) ? "true" : "false";
+echo UnityWebPortal\lib\UnitySite::testValidSSHKey($_POST["key"]) ? "true" : "false";
