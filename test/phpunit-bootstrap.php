110bd5ddf8ce418ece485c47cb78b39b335e4a77
Simon Leary <simon.leary42@proton.me>
2025-04-22 00:25:31 -0400

    add missing require

--- test/phpunit-bootstrap.php
+++ test/phpunit-bootstrap.php
@@ -13,6 +13,7 @@ require_once __DIR__ . "/../resources/lib/UnitySite.php";
 require_once __DIR__ . "/../resources/lib/UnityConfig.php";
 require_once __DIR__ . "/../resources/lib/UnityWebhook.php";
 require_once __DIR__ . "/../resources/lib/UnityRedis.php";
+require_once __DIR__ . "/../resources/lib/UnityGithub.php";
 
 global $HTTP_HEADER_TEST_INPUTS;
 $HTTP_HEADER_TEST_INPUTS = [
