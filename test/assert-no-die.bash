fe9ea0f4622995e7f6a538323fa9e6593d7f10be
Simon Leary <simon.leary42@proton.me>
2025-04-29 17:14:12 -0400

    no color in die checker

--- test/assert-no-die.bash
+++ test/assert-no-die.bash
@@ -4,7 +4,8 @@ if [[ $# -lt 1 ]]; then
     echo "at least one argument required" >&2
     exit 1
 fi
-occurrences="$(grep -H --line-number --color=always -P '\bdie\s*\(' "$@" | grep -v -P 'UnitySite::die\s*\(')" || true
+# --color=never because magit git output log doesn't support it
+occurrences="$(grep -H --color=never --line-number -P '\bdie\s*\(' "$@" | grep -v -P 'UnitySite::die\s*\(')" || true
 if [ -n "$occurrences" ]; then
     echo "die() is not allowed! use UnitySite::die() instead." >&2
     echo "$occurrences"
