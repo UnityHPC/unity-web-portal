c0a3163b45062b75c25ce1edb00b69bc6b3b79eb
Simon Leary <simon.leary42@proton.me>
2025-04-29 17:28:45 -0400

    refactor

--- test/assert-no-die-exit.bash
+++ test/assert-no-die-exit.bash
@@ -6,17 +6,19 @@ if [[ $# -lt 1 ]]; then
 fi
 
 # --color=never because magit git output log doesn't support it
-die_occurrences="$(grep -H --color=never --line-number -P '\bdie\s*\(' "$@" | grep -v -P 'UnitySite::die\s*\(')" || true
+die_occurrences="$(
+    grep -H --color=never --line-number -P '\bdie\s*[\(;]' "$@" | grep -v -P 'UnitySite::die'
+)" || true
 if [ -n "$die_occurrences" ]; then
-    echo "die() is not allowed! use UnitySite::die() instead."
+    echo "die is not allowed! use UnitySite::die() instead."
     echo "$die_occurrences"
     exit 1
 fi
 
 # --color=never because magit git output log doesn't support it
-exit_occurrences="$(grep -H --color=never --line-number -P '\exit\s*\(' "$@")" || true
+exit_occurrences="$(grep -H --color=never --line-number -P '\bexit\s*[\(;]' "$@")" || true
 if [ -n "$exit_occurrences" ]; then
-    echo "exit() is not allowed!"
+    echo "exit is not allowed! use UnitySite::die() instead."
     echo "$exit_occurrences"
     exit 1
 fi
