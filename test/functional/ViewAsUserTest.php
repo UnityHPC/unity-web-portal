7b4b4a17e4d165b203907db118c7bf2b9df068d2
Simon Leary <simon.leary42@proton.me>
2025-04-29 15:45:19 -0400

    update ViewAsUserTest

--- test/functional/ViewAsUserTest.php
+++ test/functional/ViewAsUserTest.php
@@ -1,24 +1,19 @@
 <?php
 
 use UnityWebPortal\lib\UnitySite;
-
+use UnityWebPortal\lib\exceptions\PhpUnitNoDieException;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\MockObject\MockBuilder;
 
 class ViewAsUserTest extends TestCase
 {
     public function testViewAsUser()
     {
-        global $USER, $CONFIG, $SITE;
+        global $USER;
         switchUser(...getAdminUser());
-        $this->assertEquals("user1_org1_test", $USER->getUID());
         $this->assertTrue($USER->isAdmin());
-        $adminUid = $USER->getUID();
-        $oldSite = $SITE;
+        $originalUid = $USER->getUID();
         try {
-            $SITE = $this->createMock(UnitySite::class);
-            $SITE->method("redirect");
             post(
                 __DIR__ . "/../../webroot/admin/user-mgmt.php",
                 [
@@ -26,27 +21,25 @@ class ViewAsUserTest extends TestCase
                     "uid" => "foobar",
                 ],
             );
-            $this->assertArrayHasKey("viewUser", $_SESSION);
-            // redirect means that php process dies and user's browser will initiate a new one
-            // this makes `require_once autoload.php` run again and init.php changes $USER
-            session_write_close();
-            get(__DIR__ . "/../../resources/init.php");
-            $SITE = $this->createMock(UnitySite::class);
-            $SITE->method("redirect");
-            // now we should be new user
-            $this->assertEquals("foobar", $USER->getUID());
+        } catch (PhpUnitNoDieException) {}
+        $this->assertArrayHasKey("viewUser", $_SESSION);
+        // redirect means that php process dies and user's browser will initiate a new one
+        // this makes `require_once autoload.php` run again and init.php changes $USER
+        session_write_close();
+        get(__DIR__ . "/../../resources/init.php");
+        // now we should be new user
+        $this->assertEquals("foobar", $USER->getUID());
+        try {
             post(
                 __DIR__ . "/../../resources/templates/header.php",
                 ["form_name" => "clearView"],
             );
-            // redirect means that php process dies and user's browser will initiate a new one
-            // this makes `require_once autoload.php` run again and init.php changes $USER
-            session_write_close();
-            get(__DIR__ . "/../../resources/init.php");
-            // now we should be back to original user
-            $this->assertEquals($adminUid, $USER->getUID());
-        } finally {
-            $SITE = $oldSite;
-        }
+        } catch (PhpUnitNoDieException) {}
+        // redirect means that php process dies and user's browser will initiate a new one
+        // this makes `require_once autoload.php` run again and init.php changes $USER
+        session_write_close();
+        get(__DIR__ . "/../../resources/init.php");
+        // now we should be back to original user
+        $this->assertEquals($originalUid, $USER->getUID());
     }
 }
