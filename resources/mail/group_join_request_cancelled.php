79a075a5f56cc7b4346bd3f957bf9af0a7140fc6
Kevin Bryan <bryank@uri.edu>
2025-05-23 11:46:49 -0400

    phpcs fix

--- resources/mail/group_join_request_cancelled.php
+++ resources/mail/group_join_request_cancelled.php
@@ -6,5 +6,6 @@ $this->Subject = "Unity Account Request Cancelled";
 
 <p>Hello,</p>
 
-<p>Your request to join group '<?php echo $data["group"]; ?>' on the Unity Cluster has been cancelled per your request.</p>
+<p>Your request to join group '<?php echo $data["group"]; ?>' on the Unity Cluster has been cancelled per your request.
+</p>
 
