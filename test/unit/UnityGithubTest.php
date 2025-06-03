3d1caa69133120c84e60698c30c81758cc3ad271
Simon Leary <simon.leary42@proton.me>
2025-06-03 10:23:43 -0400

    update simon key

--- test/unit/UnityGithubTest.php
+++ test/unit/UnityGithubTest.php
@@ -17,7 +17,7 @@ class UnityGithubTest extends TestCase
             # user with no keys
             ["sheldor1510", []],
             # user with 1 key
-            ["simonLeary42", ["ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL2oMxcq04PWw1iB2ZQZezFPGmX2HEKhHD6kLIoLz1RUKTNN7Glw2iF5uMFnKxYgTvdfrNjrvvLnOXvhPBvjeec="]]
+            ["simonLeary42", ["ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLeHpW10CCamJtXNXJui49WM07wRnQbQTbQ2MSvF4j8vBpBuAbjiEp14qERLDs3FoWdpbiUwL9mZq6PmUSxaTnk= simon"]]
         ];
     }
 
