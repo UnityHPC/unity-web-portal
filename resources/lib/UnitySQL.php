c1760ff82f5fe4f4d2da080726be2b7fa65910de
Simon Leary <simon.leary42@proton.me>
2025-06-07 17:00:04 -0400

    wording

--- resources/lib/UnitySQL.php
+++ resources/lib/UnitySQL.php
@@ -150,7 +150,7 @@ class UnitySQL
             throw new UnitySQLRecordNotFound("no such request: uid='$user' request_for='$dest'");
         }
         if (count($results) > 1) {
-            throw new Exception("too many requests for uid='$user' request_for='$dest'");
+            throw new Exception("multiple requests for uid='$user' request_for='$dest'");
         }
         return $results[0];
     }
