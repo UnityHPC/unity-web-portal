fb5ac2bd3a17b6aeebd8b9b1081e6fa30d051975
Simon Leary <simon.leary42@proton.me>
2025-07-06 18:47:38 -0400

    query only for attributes that we need

--- resources/lib/UnityLDAP.php
+++ resources/lib/UnityLDAP.php
@@ -257,11 +257,11 @@ class UnityLDAP extends ldapConn
         }
     }
 
-    public function getAllUsersEntries()
+    public function getAllUsersEntries($attributes = [])
     {
         $include_uids = $this->getAllUsersUIDs();
         $user_entries = $this->baseOU->getChildrenArray(
-            [], // all attributes
+            $attributes,
             true, // recursive
             "objectClass=posixAccount"
         );
@@ -322,7 +322,7 @@ class UnityLDAP extends ldapConn
         );
     }
 
-    public function getAllPIGroupOwnerEntries()
+    public function getAllPIGroupOwnerEntries($attributes = [])
     {
         // get the PI groups, filter for just the GIDs, then map the GIDs to owner UIDs
         $owner_uids = array_map(
@@ -332,7 +332,7 @@ class UnityLDAP extends ldapConn
                 $this->pi_groupOU->getChildrenArray(["cn"]),
             ),
         );
-        $owner_entries = $this->getAllUsersEntries();
+        $owner_entries = $this->getAllUsersEntries($attributes);
         foreach ($owner_entries as $i => $entry) {
             if (!in_array($entry["uid"][0], $owner_uids)) {
                 unset($owner_entries[$i]);
