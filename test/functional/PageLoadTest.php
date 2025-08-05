4fdf6fcfc9205664e05ecfdbe388840394a60df3
Simon Leary <simon.leary42@proton.me>
2025-08-05 14:37:45 -0400

    add page load tests

---
+++ test/functional/PageLoadTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use UnityWebPortal\lib\exceptions\PhpUnitNoDieException;
+use UnityWebPortal\lib\UnityGroup;
+use UnityWebPortal\lib\UnityOrg;
+use UnityWebPortal\lib\UnitySQL;
+
+class NewUserTest extends TestCase
+{
+    public static function provider()
+    {
+        $admin = getAdminUser();
+        $normal_user = getNormalUser();
+        $pi = getUserIsPIHasNoMembersNoMemberRequests();
+        return [
+            [$admin, __DIR__ . "/../../webroot/admin/pi-mgmt.php"],
+            [$admin, __DIR__ . "/../../webroot/admin/user-mgmt.php"],
+            [$admin, __DIR__ . "/../../webroot/admin/content.php"],
+            [$admin, __DIR__ . "/../../webroot/admin/notices.php"],
+            [$normal_user, __DIR__ . "/../../webroot/panel/account.php"],
+            [$normal_user, __DIR__ . "/../../webroot/panel/new_account.php"],
+            [$normal_user, __DIR__ . "/../../webroot/panel/groups.php"],
+            [$normal_user, __DIR__ . "/../../webroot/panel/support.php"],
+            [$pi, __DIR__ . "/../../webroot/panel/pi.php"],
+        ];
+    }
+
+    #[DataProvider("provider")]
+    public function testLoadPage($user, $path)
+    {
+        global $USER, $SSO, $LDAP, $SQL, $MAILER, $REDIS, $WEBHOOK;
+        switchuser($user);
+        http_get($path);
+    }
+}
