c88d399a59e1cc5ece619f1023a445850cf45015
Simon Leary <simon.leary42@proton.me>
2025-08-20 14:41:30 -0400

    add missing import

--- resources/autoload.php
+++ resources/autoload.php
@@ -24,6 +24,7 @@ require_once __DIR__ . "/lib/UnityConfig.php";
 require_once __DIR__ . "/lib/UnityWebhook.php";
 require_once __DIR__ . "/lib/UnityRedis.php";
 require_once __DIR__ . "/lib/UnityGithub.php";
+require_once __DIR__ . "/lib/exceptions/SSOException.php";
 
 // run init script
 require __DIR__ . "/init.php";
