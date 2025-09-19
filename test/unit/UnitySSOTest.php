7b6b9624e39571b994b9a88471dd082202f076b2
Simon Leary <simon.leary42@proton.me>
2025-09-19 16:39:46 -0400

    add tests for eppnToUid, eppnToOrg

--- test/unit/UnitySSOTest.php
+++ test/unit/UnitySSOTest.php
@@ -3,6 +3,7 @@
 namespace UnityWebPortal\lib;
 
 use UnityWebPortal\lib\exceptions\SSOException;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class UnitySSOTest extends TestCase
@@ -42,6 +43,84 @@ class UnitySSOTest extends TestCase
         $this->expectException(SSOException::class);
         try {
             $_SERVER["REMOTE_USER"] = "";
+            $sso = UnitySSO::getSSO();
+        } finally {
+            $_PREVIOUS_SERVER = $_SERVER;
+        }
+    }
+
+    public function testFallbackAttribute()
+    {
+        $PREVIOUS_SERVER = $_SERVER;
+        try {
+            $_SERVER["REMOTE_USER"] = "foobar@baz";
+            $_SERVER["givenName"] = "bar";
+            $_SERVER["sn"] = "bar";
+            $_SERVER["eppn"] = "foobar@baz";
+            unset($_SERVER["mail"]);
+            $sso = UnitySSO::getSSO();
+        } finally {
+            $_PREVIOUS_SERVER = $_SERVER;
+        }
+        $this->assertEquals("foobar@baz", $sso["mail"]);
+    }
+
+    public static function validEppnProvider() {
+        return [
+            ["foo@bar.edu", "foo_bar_edu", "bar_edu"],
+        ];
+    }
+
+    #[DataProvider("validEppnProvider")]
+    public function testEppnToUid(string $eppn, string $expectedUID, string $_)
+    {
+        $PREVIOUS_SERVER = $_SERVER;
+        try {
+            $_SERVER["REMOTE_USER"] = $eppn;
+            $_SERVER["givenName"] = "foo";
+            $_SERVER["sn"] = "foo";
+            $_SERVER["mail"] = "foo";
+            $sso = UnitySSO::getSSO();
+            $this->assertEquals($expectedUID, $sso["user"]);
+        } finally {
+            $_PREVIOUS_SERVER = $_SERVER;
+        }
+    }
+
+    #[DataProvider("validEppnProvider")]
+    public function testEppnToOrg(string $eppn, string $_, string $expectedOrg)
+    {
+        $PREVIOUS_SERVER = $_SERVER;
+        try {
+            $_SERVER["REMOTE_USER"] = $eppn;
+            $_SERVER["givenName"] = "foo";
+            $_SERVER["sn"] = "foo";
+            $_SERVER["mail"] = "foo";
+            $sso = UnitySSO::getSSO();
+            $this->assertEquals($expectedOrg, $sso["org"]);
+        } finally {
+            $_PREVIOUS_SERVER = $_SERVER;
+        }
+    }
+
+    public static function invalidEppnProvider() {
+        return [
+            ["foo"], // missing @
+            ["foo@bar@baz"], // too many @
+            [""]
+        ];
+    }
+
+    #[DataProvider("invalidEppnProvider")]
+    public function testInvalidEPPN(string $eppn)
+    {
+        $this->expectException(SSOException::class);
+        $PREVIOUS_SERVER = $_SERVER;
+        try {
+            $_SERVER["REMOTE_USER"] = $eppn;
+            $_SERVER["givenName"] = "foo";
+            $_SERVER["sn"] = "foo";
+            $_SERVER["mail"] = "foo";
             $sso = UnitySSO::getSSO();
         } finally {
             $_PREVIOUS_SERVER = $_SERVER;
