7699c791c0ed285091dfc1eacc20d1fd17ac3105
Simon Leary <simon.leary42@proton.me>
2025-09-30 10:09:51 -0400

    improve convention scripts

--- test/assert-UnityHTTPD-used.bash
+++ test/assert-UnityHTTPD-used.bash
@@ -1,28 +1,22 @@
-#!/bin/bash
 set -euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 if [[ $# -lt 1 ]]; then
     echo "at least one argument required"
     exit 1
 fi
 
+funcs=(die exit)
 rc=0
-
-# --color=never because magit git output log doesn't support it
-die_occurrences="$(
-    grep -H --color=never --line-number -P '\bdie\s*[\(;]' "$@" | grep -v -P 'UnityHTTPD::die'
-)" || true
-if [ -n "$die_occurrences" ]; then
-    echo "die is not allowed! use UnityHTTPD::die() instead."
-    echo "$die_occurrences"
-    rc=1
-fi
-
-# --color=never because magit git output log doesn't support it
-exit_occurrences="$(grep -H --color=never --line-number -P '\bexit\s*[\(;]' "$@")" || true
-if [ -n "$exit_occurrences" ]; then
-    echo "exit is not allowed! use UnityHTTPD::die() instead."
-    echo "$exit_occurrences"
-    rc=1
-fi
-
+for func in "${funcs[@]}"; do
+    # --color=never because magit git output log doesn't support it
+    grep_rc=0; grep -H --color=never --line-number -P '\b'"$func"'\s*[\(;]' "$@" | grep -v -E 'UnityHTTPD::'"$func" || grep_rc=$?
+    case "$grep_rc" in
+        0)
+            echo "$func() are not allowed! use UnityHTTPD::die() instead."; rc=1 ;;
+        1)
+            : ;; # code is good, do nothing
+        *)
+            echo "grep failed!";  rc=1 ;;
+    esac
+done
 exit "$rc"
--- test/assert-exceptions-used.bash
+++ test/assert-exceptions-used.bash
@@ -1,18 +1,22 @@
-#!/usr/bin/env bash
 set -euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 if [[ $# -lt 1 ]]; then
     echo "at least one argument required"
     exit 1
 fi
 
-err_funcs=(badRequest forbidden internalServerError)
-
+funcs=(badRequest forbidden internalServerError)
 rc=0
-for err_func in "${err_funcs[@]}"; do
+for func in "${funcs[@]}"; do
     # --color=never because magit git output log doesn't support it
-    if grep -H --color=never --line-number -P '\b'"$err_func"'\s*[\(;]' "$@"; then
-        echo "$err_func() is not allowed! use an exceptioninstead."
-        rc=1
-    fi
+    grep_rc=0; grep -H --color=never --line-number -P '\b'"$func"'\s*[\(;]' "$@" || grep_rc=$?
+    case "$grep_rc" in
+        0)
+            echo "$func() is not allowed! use an exception instead."; rc=1 ;;
+        1)
+            : ;; # code is good, do nothing
+        *)
+            echo "grep failed!";  rc=1 ;;
+    esac
 done
 exit "$rc"
