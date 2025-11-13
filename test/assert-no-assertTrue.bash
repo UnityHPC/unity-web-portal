7bb1900906d2cab3f86b9850685f63b4cc9196e3
Simon Leary <simon.leary42@proton.me>
2025-11-13 14:10:18 -0500

    no loop

--- test/assert-no-assertTrue.bash
+++ test/assert-no-assertTrue.bash
@@ -5,18 +5,15 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-funcs=(assertTrue)
 rc=0
-for func in "${funcs[@]}"; do
-    # --color=never because magit git output log doesn't support it
-    grep_rc=0; grep -H --color=never --line-number -P '\b'"$func"'\s*[\(;]' "$@" || grep_rc=$?
-    case "$grep_rc" in
-        0)
-            echo "$func() is not allowed! use an exception instead."; rc=1 ;;
-        1)
-            : ;; # code is good, do nothing
-        *)
-            echo "grep failed!";  rc=1 ;;
-    esac
-done
+# --color=never because magit git output log doesn't support it
+grep_rc=0; grep -H --color=never --line-number -P '\bassertTrue\s*[\(;]' "$@" || grep_rc=$?
+case "$grep_rc" in
+    0)
+        echo "assertTrue() is not allowed! use assert() instead."; rc=1 ;;
+    1)
+        : ;; # code is good, do nothing
+    *)
+        echo "grep failed!";  rc=1 ;;
+esac
 exit "$rc"
