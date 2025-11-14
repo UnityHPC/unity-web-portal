fca9acd47742e704bc2ef84608893e71b6fc24db
Simon Leary <simon.leary42@proton.me>
2025-11-14 14:15:50 -0500

    update markup script

--- test/phpunit-markup.php
+++ test/phpunit-markup.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php while ($line = fgets(STDIN)) {
     $line = rtrim($line, "\n");
-    if (preg_match('/\s*^(\/.*):(\d+)$/', $line, $matches)) {
-        [$path, $lineNumber] = [$matches[1], intval($matches[2]) - 1];
+    if (preg_match('/^(\d+\))?\s*(\/.*):(\d+)$/', $line, $matches)) {
+        [$path, $lineNumber] = [$matches[2], intval($matches[3]) - 1];
         $src_line = trim(file($path)[$lineNumber]);
         echo "$line \033[36m$src_line\033[0m\n";
     } else {
