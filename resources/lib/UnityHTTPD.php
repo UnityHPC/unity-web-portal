45646a537573ce3f3b11a30d55477b07dcc6182e
Simon Leary <simon.leary42@proton.me>
2025-12-11 13:19:23 -0500

    whitespace

--- resources/lib/UnityHTTPD.php
+++ resources/lib/UnityHTTPD.php
@@ -48,10 +48,12 @@ class UnityHTTPD
         self::die();
     }
 
-    /* generate a unique ID that can be logged and also displayed to the user when something has
+    /*
+    generate a unique ID that can be logged and also displayed to the user when something has
     gone wrong. This way the user can give us the ID and we can find the exact line in the log
     file that represents their issue.
-    assumes that a session is started and has a good unique session_id */
+    assumes that a session is started and has a good unique session_id
+    */
     public static function errorID(?\Throwable $e = null): string
     {
         $e ??= new stdClass();
