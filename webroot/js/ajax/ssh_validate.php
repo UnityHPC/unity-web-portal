b5cc2125119967d4a7b5e4d698c70357603e487f
Simon Leary <simon.leary42@proton.me>
2025-12-18 10:30:04 -0500

    add missing import

--- webroot/js/ajax/ssh_validate.php
+++ webroot/js/ajax/ssh_validate.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . "/../../../vendor/autoload.php";
 require_once __DIR__ . "/../../../resources/lib/utils.php";
+require_once __DIR__ . "/../../../resources/lib/UnityHTTPD.php";
 
 use UnityWebPortal\lib\UnityHTTPD;
 
