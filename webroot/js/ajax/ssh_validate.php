36ce3ed409f375aceb847b85950b1f8c1dc84844
Simon Leary <simon.leary42@proton.me>
2025-12-18 10:20:04 -0500

    handle missing post data

--- webroot/js/ajax/ssh_validate.php
+++ webroot/js/ajax/ssh_validate.php
@@ -2,5 +2,8 @@
 
 require_once __DIR__ . "/../../../vendor/autoload.php";
 require_once __DIR__ . "/../../../resources/lib/utils.php";
+
+use UnityWebPortal\lib\UnityHTTPD;
+
 header('Content-Type: application/json; charset=utf-8');
-echo jsonEncode(["is_valid" => testValidSSHKey($_POST["key"])]);
+echo jsonEncode(["is_valid" => testValidSSHKey(UnityHTTPD::getPostData("key"))]);
