605c3b923b7acd4a0ec72983e5c53fb0c7683a7a
Simon Leary <simon.leary42@proton.me>
2025-12-29 11:39:50 -0500

    reorder list

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -30,7 +30,7 @@
 
 </body>
 <?php
-foreach (["filter", "sort", "tables", "messages", "global-late"] as $x) {
+foreach (["filter", "sort", "global-late", "tables", "messages"] as $x) {
     $url = getURL("js/$x.js?v=" . CONFIG["upstream"]["version"]);
     echo "<script src='$url'></script>";
 }
