bf78a98bb6a57897e5956dedcfc40585d2f0c098
Simon Leary <simon.leary42@proton.me>
2026-01-05 14:23:38 -0500

    more page load tests

--- test/functional/PageLoadTest.php
+++ test/functional/PageLoadTest.php
@@ -1,10 +1,54 @@
 <?php
 
 use PHPUnit\Framework\Attributes\DataProvider;
+use UnityWebPortal\lib\exceptions\NoDieException;
+use TRegx\PhpUnit\DataProviders\DataProvider as TRegxDataProvider;
 
 class PageLoadTest extends UnityWebPortalTestCase
 {
-    public static function provider()
+    public static function findPHPFiles($path)
+    {
+        // if I do this recursively I get the ajax and modal files, which aren't appropriate
+        // for these tests, so instead I just list the directory
+        // $directory_iterator = new RecursiveDirectoryIterator($path);
+        // $iterator_iterator = new RecursiveIteratorIterator($directory_iterator);
+        // $regex_iterator = new RegexIterator(
+        //     $iterator_iterator,
+        //     '/^.+\.php$/i',
+        //     RecursiveRegexIterator::GET_MATCH
+        // );
+        // return array_keys(iterator_to_array($regex_iterator)));
+        $files = [];
+        foreach (new DirectoryIterator($path) as $file) {
+            if (!$file->isDot() && str_ends_with($file->getFilename(), ".php")) {
+                array_push($files, join("/", [$path, $file->getFilename()]));
+            }
+        }
+        return $files;
+    }
+
+    public static function panelPages()
+    {
+        return TRegxDataProvider::list(...self::findPHPFiles(__DIR__ . "/../../webroot/panel"));
+    }
+
+    public static function adminPages()
+    {
+        return TRegxDataProvider::list(...self::findPHPFiles(__DIR__ . "/../../webroot/admin"));
+    }
+
+    public static function providerNonexistentUser()
+    {
+        $dir = __DIR__ . "/../../webroot/panel";
+        $excludePages = array_map(fn($x) => "$dir/$x.php", [
+            "pi", // requires user to be a PI
+            "new_account", // this is the one page that does not redirect to new_account
+        ]);
+        $panelPages = self::findPHPFiles($dir);
+        $output = array_diff($panelPages, $excludePages);
+        return TRegxDataProvider::list(...$output);
+    }
+    public static function pagesAndRegexes()
     {
         return [
             // normal page load
@@ -17,24 +61,34 @@ class PageLoadTest extends UnityWebPortalTestCase
             ["Blank", "panel/groups.php", "/My Principal Investigators/"],
             ["Blank", "panel/support.php", "/Support/"],
             ["EmptyPIGroupOwner", "panel/pi.php", "/My Users/"],
-            // normal user should not be able to access admin pages
-            ["Blank", "admin/pi-mgmt.php", "/You are not an admin/"],
-            ["Blank", "admin/user-mgmt.php", "/You are not an admin/"],
-            ["Blank", "admin/content.php", "/You are not an admin/"],
-            ["Blank", "admin/notices.php", "/You are not an admin/"],
             // new_account.php should redirect to account.php if account already exists
             ["Blank", "panel/new_account.php", "/panel\/account\.php/"],
-            // all pages should redirect to new_account.php if account does not exist
-            ["NonExistent", "panel/account.php", "/panel\/new_account\.php/"],
+            // non-PI can't access pi.php
+            ["Blank", "panel/pi.php", "/You are not a PI./"],
         ];
     }
 
-    #[DataProvider("provider")]
+    #[DataProvider("pagesAndRegexes")]
     public function testLoadPage($nickname, $path, $regex)
     {
-        global $USER, $SSO, $LDAP, $SQL, $MAILER, $WEBHOOK;
         $this->switchUser($nickname);
         $output = http_get(__DIR__ . "/../../webroot/" . $path);
         $this->assertMatchesRegularExpression($regex, $output);
+    }
+
+    #[DataProvider("adminPages")]
+    public function testLoadAdminPageNotAnAdmin($path)
+    {
+        $this->switchUser("Blank");
+        $output = http_get($path);
+        $this->assertMatchesRegularExpression("/You are not an admin\./", $output);
+    }
+
+    #[DataProvider("providerNonexistentUser")]
+    public function testLoadPageNonexistentUser($path)
+    {
+        $this->switchUser("NonExistent");
+        $output = http_get($path);
+        $this->assertMatchesRegularExpression("/panel\/new_account\.php/", $output);
     }
 }
