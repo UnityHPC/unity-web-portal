eb1cb09c6b7d86e8c74dcbb3ebef2a5ccff4d1db
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2026-01-12 14:54:23 -0500

    install garden for making CLIs (#518)

--- workers/remove-users-from-group.php
+++ workers/remove-users-from-group.php
@@ -1,19 +1,18 @@
 #!/usr/bin/env php
 <?php
 include __DIR__ . "/init.php";
+use Garden\Cli\Cli;
 use UnityWebPortal\lib\UnityUser;
 use UnityWebPortal\lib\UnityGroup;
 
-$help = sprintf("Usage: %s group_name filename_of_users_to_remove\n", $argv[0]);
-if (count(array_intersect($argv, ["-h", "--help"])) > 0) {
-    _die($help, 0);
-}
-if (sizeof($argv) != 3) {
-    _die($help, 1);
-}
+$cli = new Cli();
+$cli->description("Remove users from a group")
+    ->arg("gid", "Group ID (example: pi_user1_org1_test)", true)
+    ->arg("users-file-path", "Path to file containing one UID per line", true);
+$args = $cli->parse($argv, true);
 
-$gid = $argv[1];
-$filename = $argv[2];
+$gid = $args->getArg("gid");
+$filename = $args->getArg("users-file-path");
 $group = new UnityGroup($gid, $LDAP, $SQL, $MAILER, $WEBHOOK);
 if (!$group->exists()) {
     _die("No such group '$gid'\n", 1);
