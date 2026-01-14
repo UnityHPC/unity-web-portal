35709cf0d3be687ecee6f8b285c13aafe945873f
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2026-01-14 12:59:27 -0500

    remove LDAPConn::search (#530)

--- resources/lib/UnityLDAP.php
+++ resources/lib/UnityLDAP.php
@@ -311,14 +311,15 @@ class UnityLDAP extends LDAPConn
     /**
      * @throws \UnityWebPortal\lib\exceptions\EntryNotFoundException
      */
-    public function getUidFromEmail(string $email): LDAPEntry
+    public function getUidFromEmail(string $email): string
     {
         $email = ldap_escape($email, "", LDAP_ESCAPE_FILTER);
-        $cn = $this->search("mail=$email", CONFIG["ldap"]["user_ou"], ["cn"]);
-        if ($cn && count($cn) == 1) {
-            return $cn[0];
+        $entries = $this->userOU->getChildrenArrayStrict(["uid"], true, "(mail=$email)");
+        if (count($entries) == 0) {
+            throw new exceptions\EntryNotFoundException($email);
+        } else {
+            return $entries[0]["uid"][0];
         }
-        throw new exceptions\EntryNotFoundException($email);
     }
 
     /**
