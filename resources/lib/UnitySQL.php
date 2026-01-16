e3e393519e69040ed3143fe0841d137feaf6496c
Simon Leary <simon.leary42@proton.me>
2026-01-16 18:30:05 -0500

    use plain Exception

--- resources/lib/UnitySQL.php
+++ resources/lib/UnitySQL.php
@@ -250,7 +250,7 @@ class UnitySQL
                 $disable = jsonDecode($record["disable_warning_days_sent"]);
                 return ["idlelock" => $idlelock, "disable" => $disable];
             default:
-                throw new RecordNotUniqueException("uid='$uid'");
+                throw new \Exception("multiple records found with uid='$uid'");
         }
     }
 
