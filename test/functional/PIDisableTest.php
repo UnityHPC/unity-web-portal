e6c133453e638b855f20d7c13a6a42f7802baeab
Simon Leary <simon.leary42@proton.me>
2026-01-16 10:13:59 -0500

    uncomment

--- test/functional/PIDisableTest.php
+++ test/functional/PIDisableTest.php
@@ -11,8 +11,7 @@ class PIDisableTest extends UnityWebPortalTestCase
         $memberuids_before = $pi_group->getMemberUIDs();
         $this->assertFalse($pi_group->getIsDisabled());
         $this->assertNotEmpty($pi_group->getMemberUIDs());
-        // FIXME uncomment
-        // $this->assertTrue($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
+        $this->assertTrue($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
         try {
             $this->switchUser("Admin");
             http_post(__DIR__ . "/../../webroot/admin/pi-mgmt.php", [
@@ -22,14 +21,12 @@ class PIDisableTest extends UnityWebPortalTestCase
             $this->assertFalse($pi_group->getOwner()->isPI());
             $this->assertTrue($pi_group->getIsDisabled());
             $this->assertEmpty($pi_group->getMemberUIDs());
-            // FIXME uncomment
-            // $this->assertFalse($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
+            $this->assertFalse($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
         } finally {
             $entry = $LDAP->getPIGroupEntry($pi_group->gid);
             $entry->setAttribute("memberuid", $memberuids_before);
             $entry->setAttribute("isDisabled", "FALSE");
-            // FIXME uncomment
-            // $pi_group->getOwner()->setFlag(UserFlag::QUALIFIED, true);
+            $pi_group->getOwner()->setFlag(UserFlag::QUALIFIED, true);
         }
     }
 
@@ -41,15 +38,13 @@ class PIDisableTest extends UnityWebPortalTestCase
         $memberuids_before = $pi_group->getMemberUIDs();
         $this->assertFalse($pi_group->getIsDisabled());
         $this->assertNotEmpty($pi_group->getMemberUIDs());
-        // FIXME uncomment
-        // $this->assertTrue($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
+        $this->assertTrue($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
         try {
             http_post(__DIR__ . "/../../webroot/panel/pi.php", ["form_type" => "disable"]);
             $this->assertFalse($pi_group->getOwner()->isPI());
             $this->assertTrue($pi_group->getIsDisabled());
             $this->assertEmpty($pi_group->getMemberUIDs());
-            // FIXME uncomment
-            // $this->assertFalse($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
+            $this->assertFalse($pi_group->getOwner()->getFlag(UserFlag::QUALIFIED));
         } finally {
             $entry = $LDAP->getPIGroupEntry($pi_group->gid);
             $entry->setAttribute("memberuid", $memberuids_before);
@@ -63,26 +58,22 @@ class PIDisableTest extends UnityWebPortalTestCase
         global $USER, $LDAP;
         $this->switchUser("Blank");
         $new_user = $USER;
-        // FIXME uncomment
-        // $this->assertFalse($new_user->getFlag(UserFlag::QUALIFIED));
+        $this->assertFalse($new_user->getFlag(UserFlag::QUALIFIED));
         $this->switchUser("EmptyPIGroupOwner");
         $pi_group = $USER->getPIGroup();
         $memberuids_before = $pi_group->getMemberUIDs();
         try {
             $pi_group->newUserRequest($new_user);
             $pi_group->approveUser($new_user);
-            // FIXME uncomment
-            // $this->assertTrue($new_user->getFlag(UserFlag::QUALIFIED));
+            $this->assertTrue($new_user->getFlag(UserFlag::QUALIFIED));
             http_post(__DIR__ . "/../../webroot/panel/pi.php", ["form_type" => "disable"]);
-            // FIXME uncomment
-            // $this->assertFalse($new_user->getFlag(UserFlag::QUALIFIED));
+            $this->assertFalse($new_user->getFlag(UserFlag::QUALIFIED));
         } finally {
             $entry = $LDAP->getPIGroupEntry($pi_group->gid);
             $entry->setAttribute("memberuid", $memberuids_before);
             $entry->setAttribute("isDisabled", "FALSE");
             $pi_group->getOwner()->setFlag(UserFlag::QUALIFIED, true);
-            // FIXME uncomment
-            // $new_user->setFlag(UserFlag::QUALIFIED, false);
+            $new_user->setFlag(UserFlag::QUALIFIED, false);
         }
     }
 
