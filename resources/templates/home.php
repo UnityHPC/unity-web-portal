1de61b70fd9a1c5b108c716dcddf4c7eca885a4f
Simon Leary <simon.leary42@proton.me>
2026-01-16 12:14:18 -0500

    tell user to log in if not logged in

--- resources/templates/home.php
+++ resources/templates/home.php
@@ -3,6 +3,14 @@
     Welcome to the UnityHPC Platform Account Portal.
     Here you can manage your SSH keys, join and leave PI groups, manage your own PI group, and more.
 </p>
+
+<?php
+if (!($_SESSION["user_exists"] ?? false)) {
+    $hyperlink = getHyperlink("Log In", "panel/account.php");
+    echo "<p>Please $hyperlink for more information.</p>";
+}
+?>
+
 <br>
 
 <h2>Cluster Notices</h2>
