68abc99d2c3a04400f5334f547174b35fc7f9a03
Simon Leary <simon.leary42@proton.me>
2026-01-21 15:44:01 -0500

    cast different

--- resources/lib/UnityLDAP.php
+++ resources/lib/UnityLDAP.php
@@ -257,7 +257,7 @@ class UnityLDAP extends LDAPConn
     public function getAllNonDisabledPIGroupOwnerUIDs(): array
     {
         return array_map(
-            fn($x) => (string) UnityGroup::GID2OwnerUID($x["cn"][0]),
+            fn($x) => UnityGroup::GID2OwnerUID((string) $x["cn"][0]),
             $this->getAllNonDisabledPIGroupsAttributes(["cn"]),
         );
     }
