a2eb342ee23e6071b03971c3de6d4cd1061470fc
Simon Leary <simon.leary42@proton.me>
2026-01-21 13:20:16 -0500

    use _fopen in worker

--- workers/remove-users-from-group.php
+++ workers/remove-users-from-group.php
@@ -17,7 +17,7 @@ $group = new UnityGroup($gid, $LDAP, $SQL, $MAILER, $WEBHOOK);
 if (!$group->exists()) {
     _die("No such group '$gid'\n", 1);
 }
-($handle = fopen($filename, "r")) or _die("Can't open '$filename'\n", 1);
+$handle = _fopen($filename, "r");
 while (($line = fgets($handle)) !== false) {
     $uid = trim($line);
     $user = new UnityUser($uid, $LDAP, $SQL, $MAILER, $WEBHOOK);
